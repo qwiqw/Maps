bb15f7ee470321eaf53468b2ac188dd40062311a
qwiqw <90405747+qwiqw@users.noreply.github.com>
2025-02-17 18:59:41 +0300

    Add files via upload
    map 2

--- 123.py
+++ 123.py
@@ -13,29 +13,44 @@ url = 'https://static-maps.yandex.ru/v1'
 params = {
     'apikey': "4e60121e-3e68-41f0-bd84-eced30775d1c",
     'll': '37.617649,55.755115',
-    'z': '15',
-
+    'z': 15,
 }
 
-response = requests.get(url=url, params=params)
 
-if not response:
-    print(f'{response.status_code}{response.reason}')
-    sys.exit(1)
+def get_map():
+    response = requests.get(url=url, params=params)
 
-with open('map.png', mode='wb') as map_file:
-    map_file.write(response.content)
+    if not response:
+        print(f'{response.status_code}{response.reason}')
+        sys.exit(1)
+
+    with open('map.png', mode='wb') as map_file:
+        map_file.write(response.content)
+
+    return pygame.image.load('map.png')
+
 
 running = True
-map_image = pygame.image.load('map.png')
+
+map_image = get_map()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_PAGEUP:
+                params['z'] += 1
+                if params['z'] >= 21:
+                    params['z'] = 21
+                map_image = get_map()
+            if event.key == pygame.K_PAGEDOWN:
+                params['z'] -= 1
+                if params['z'] <= 1:
+                    params['z'] = 1
+                map_image = get_map()
 
     screen.blit(map_image, (0, 0))
     pygame.display.flip()
 os.remove('map.png')
 
 pygame.quit()
-
